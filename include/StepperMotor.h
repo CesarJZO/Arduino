4537b30e7e99c180406ac4b56c266639a04f7e98
CesarJZO <cesarjzo@outlook.com>
2022-10-06 21:24:01 -0500

    Add stepper

--- include/StepperMotor.h
+++ include/StepperMotor.h
@@ -1,20 +1,89 @@
-#include "Circuit.h"
+#include <Circuit.h>
 
-/**
- *
- */
 class StepperMotor : Circuit
 {
 private:
     int currentState;
 
+    /**
+     * Simple step clock wise
+     */
+    const int simpleStepCW[4][4] = {
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}};
+
+    /**
+     * Simple step counter-clock wise
+     */
+    const int simpleStepCC[4][4] = {
+        {0, 0, 0, 1},
+        {0, 0, 1, 0},
+        {0, 1, 0, 0},
+        {1, 0, 0, 0}};
+
+    /**
+     * Full step clock wise
+     */
+    const int fullStepCW[4][4] = {
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 1},
+        {1, 0, 0, 1}};
+
+    /**
+     * Full step counter-clock wise
+     */
+    const int fullStepCC[4][4] = {
+        {0, 0, 1, 1},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 1}};
+
+    /**
+     * Half step clock wise configuration
+     */
+    const int halfStepCW[8][4] = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 1},
+        {1, 0, 0, 1}};
+
+    /**
+     * Half step counter-clock wise
+     */
+    const int halfStepCC[8][4] = {
+        {0, 0, 0, 1},
+        {0, 0, 1, 1},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 1}};
+
 public:
     /**
-     *
+     * Sets the input and output pins
      */
     void setup() override;
     /**
-     *
+     * Depending on the dip switch config, it will run different steps
      */
     void loop() override;
+
+private:
+    /**
+     * Executes the steps
+     * @param config The type of steps to run
+     * @param steps The number of steps
+     */
+    void step(const int config[][4], int steps);
+
+    int8_t getBinaryValue(int pin0, int pin1, int pin2);
 };
--- src/StepperMotor.cpp
+++ src/StepperMotor.cpp
@@ -2,36 +2,55 @@
 #include <nanopins.h>
 #include <StepperMotor.h>
 
-int delayTime = 50;
-
 void StepperMotor::setup()
 {
-    pinMode(D2, OUTPUT);
-    pinMode(D3, OUTPUT);
-    pinMode(D4, OUTPUT);
-    pinMode(D5, OUTPUT);
+    for (int p = D2; p <= D8; p++)
+        pinMode(p, p < D5 ? INPUT : OUTPUT);
 }
 
 void StepperMotor::loop()
 {
-    digitalWrite(D2, HIGH);
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, LOW);
-    digitalWrite(D5, LOW);
-    delay(delayTime);
-    digitalWrite(D2, LOW);
-    digitalWrite(D3, HIGH);
-    digitalWrite(D4, LOW);
-    digitalWrite(D5, LOW);
-    delay(delayTime);
-    digitalWrite(D2, LOW);
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, HIGH);
-    digitalWrite(D5, LOW);
-    delay(delayTime);
-    digitalWrite(D2, LOW);
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, LOW);
-    digitalWrite(D5, HIGH);
-    delay(delayTime);
+    currentState = getBinaryValue(D4, D3, D2);
+    
+    switch (currentState)
+    {
+        default:break;
+        case 0x001: step(simpleStepCW, 4); break;
+        case 0x010: step(simpleStepCC, 4); break;
+        case 0x011: step(fullStepCW, 4); break;
+        case 0x100: step(fullStepCC, 4); break;
+        case 0x101: step(halfStepCW, 8); break;
+        case 0x110: step(halfStepCC, 8); break;
+    }
+}
+
+int8_t StepperMotor::getBinaryValue(int pin0, int pin1, int pin2)
+{
+    int bin = 0;
+
+    if (digitalRead(pin0) == HIGH)
+        bin += 1;
+    if (digitalRead(pin1) == HIGH)
+        bin += 2;
+    if (digitalRead(pin2) == HIGH)
+        bin += 4;
+    
+    return bin;
+}
+
+void StepperMotor::step(const int config[][4], int steps)
+{
+    for (int i = 0; i < 512; i++)
+    {
+        for (int i = 0; i < steps; i++)
+        {
+            for (int p = D5; p <= D8; p++)
+                digitalWrite(p, config[i][p - 5]);
+            delay(12);
+        }
+    }
+
+    for (int p = D5; p <= D8; p++)
+        digitalWrite(p, LOW);
+    delay(2000);
 }
